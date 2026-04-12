b5b625256d7f961467437e9d3071e45c1292d870
Daniel McCave <mccavedaniel@icloud.com>
2026-04-11 21:00:37 -0700

    Update Calculator.py
    refactored Basic Calculator into classes using OOP (Object Oriented Programming)

--- Calculator.py
+++ Calculator.py
@@ -1,37 +1,87 @@
-while True:
-    print()
-    print("Python Calculator")
-    print("=" * 20)
-    print()
 
-    num1 = float(input("Enter the first number: "))
-    num2 = float(input("Enter the second number: "))
+class Calculator:
+    def __init__(self):
+        self.operations = Operations()
+
+    def add(self, a, b):
+        return self.operations.add(a, b)
     
-    operator = input("Enter an operation (+, -, *, /, **): ")
-    print(f"You chose: {operator}")
-    print()
-    if (operator == "+"):
-        result = num1 + num2
-        print(f"{num1} + {num2} = {result}")
-    elif (operator == "-"):
-        result = num1 - num2
-        print(f"{num1} - {num2} = {result}")
-    elif (operator == "*"):
-        result = num1 * num2
-        print(f"{num1} * {num2} = {result}")
-    elif (operator == "**"):
-        result = num1 ** num2
-        print(f"{num1} ** {num2} = {result}")
-    elif operator == "/" and num2 != 0:
-        result = num1 / num2
-        print(f"{num1} / {num2} = {result}")
-    elif operator == "/" and num2 == 0:
-        print("Cannot divide by Zero!")
-    else:
-        print("Invalid Operator!")
-        print()
-    reset = input("Press 'C' to calculate again. Press any other key to exit! ")
-    if reset.lower() != "c":
-        print("GOODBYE!")
-        print()
-        break
+    def subtract(self, a, b):
+        return self.operations.subtract(a, b)
+    
+    def multiply(self, a, b):
+        return self.operations.multiply(a, b)
+    
+    def divide(self, a, b):
+        return self.operations.divide(a, b)
+
+
+
+class Operations:
+    def add(self, a, b):
+        return a + b
+    
+    def subtract(self, a, b):
+        return a - b
+    
+    def multiply(self, a, b):
+        return a * b
+    
+    def divide(self, a, b):
+        if b == 0:
+            raise ValueError("Cannot divide by zero!")
+        else:
+            return a / b
+        
+
+class Menu:
+    def display(self):
+        print("\n-----Calculator-----\n")
+        print('''
+            1. Add
+            2. Subtract
+            3. Multiply
+            4. Divide
+            5. Exit
+        ''')
+
+    def get_choice(self):
+        return input("Please select a option from the menu: ")
+    
+    def get_numbers(self):
+        a = float(input("Enter the first number: "))
+        b = float(input("Enter the second number: "))
+        return a, b
+    
+class App:
+    def __init__(self):
+        self.calculator = Calculator()
+        self.menu = Menu()
+
+    def run(self):
+        while True:
+            self.menu.display()
+            choice = self.menu.get_choice()
+
+            if choice == "5":
+                print("You selected to end the program! Have A Great Day!")
+                break
+
+            a, b = self.menu.get_numbers()
+
+            if choice == "1":
+                print(self.calculator.add(a, b))
+
+            elif choice == "2":
+                print(self.calculator.subtract(a, b))
+
+            elif choice == "3":
+                print(self.calculator.multiply(a, b))
+
+            elif choice == "4":
+                print(self.calculator.divide(a, b))
+
+            else:
+                print("Invalid choice. Please enter a option from menu.")
+
+App().run()
